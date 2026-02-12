0ea252b2244fc6aaf656c9b027ea297ef39efbcd
chengda <chengda.cn@gmail.com>
2026-02-12 17:40:34 +0800

    修改启动脚本

--- ztrader.sh
+++ ztrader.sh
@@ -4,15 +4,15 @@
 cd "$(dirname "$0")"
 
 # 定义输出目录和主类
-OUTPUT_DIR="output"
+LIB_DIR="lib"
 MAIN_CLASS="org.zenoda.ztrader.ZtraderServer"
 
 # 查找output目录下所有的jar文件
-JAR_FILES=$(find $OUTPUT_DIR -name "*.jar" | paste -sd ":")
+JAR_FILES=$(find $LIB_DIR -name "*.jar" | paste -sd ":")
 
 # 检查是否找到了jar文件
 if [ -z "$JAR_FILES" ]; then
-  echo "没有在$output目录中找到任何jar文件。"
+  echo "没有在$LIB_DIR目录中找到任何jar文件。"
   exit 1
 fi
 
