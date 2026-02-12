34837dff43901d142d67cef44853e9fa77beb685
chengda <chengda.cn@gmail.com>
2026-02-12 17:38:01 +0800

    优化构建脚本，添加启动脚本

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 CLIENT_DIR="$SCRIPT_DIR/ztrader-client"
 SERVER_DIR="$SCRIPT_DIR/ztrader-server"
 OUTPUT_DIR="$SCRIPT_DIR/output"
+LIB_DIR="$OUTPUT_DIR/lib"
 
 # 从 package.json 中获取前端版本号
 get_frontend_version() {
@@ -24,7 +25,7 @@ get_backend_version() {
 
 # 清理 output 目录
 clean_output_directory() {
-  rm -rf "$OUTPUT_DIR" && mkdir -p "$OUTPUT_DIR"
+  rm -rf "$OUTPUT_DIR" && mkdir -p "$LIB_DIR"
 }
 
 # 构建前端
@@ -34,17 +35,31 @@ build_frontend() {
 
 # 打包前端
 package_frontend() {
-  (cd "$CLIENT_DIR/dist" && jar cf "$OUTPUT_DIR/ztrader-client-$FRONTEND_VERSION.jar" *)
+  # 创建临时目录并将前端构建结果复制到 static 目录下
+  TEMP_DIR=$(mktemp -d)
+  mkdir -p "$TEMP_DIR/static"
+  cp -r "$CLIENT_DIR/dist"/* "$TEMP_DIR/static"
+  
+  # 打包前端
+  (cd "$TEMP_DIR" && jar cf "$LIB_DIR/ztrader-client-$FRONTEND_VERSION.jar" *)
+  
+  # 清理临时目录
+  rm -rf "$TEMP_DIR"
 }
 
 # 构建后端
 build_backend() {
-  (cd "$SERVER_DIR" && mvn clean package -DskipTests && mvn dependency:copy-dependencies -DoutputDirectory="$OUTPUT_DIR")
+  (cd "$SERVER_DIR" && mvn clean package -DskipTests && mvn dependency:copy-dependencies -DoutputDirectory="$LIB_DIR")
 }
 
 # 移动后端构建结果
 move_backend_jar() {
-  find "$SERVER_DIR/target" -name "*.jar" -exec mv {} "$OUTPUT_DIR/ztrader-server-$BACKEND_VERSION.jar" \;
+  find "$SERVER_DIR/target" -name "*.jar" -exec mv {} "$LIB_DIR/ztrader-server-$BACKEND_VERSION.jar" \;
+}
+
+# 复制脚本到 output 目录
+copy_script_to_output() {
+  cp "$SCRIPT_DIR/ztrader.sh" "$OUTPUT_DIR/ztrader.sh"
 }
 
 # 主流程
@@ -55,3 +70,4 @@ build_frontend
 package_frontend
 build_backend
 move_backend_jar
+copy_script_to_output
---
+++ ztrader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 设置工作目录为当前脚本所在目录
+cd "$(dirname "$0")"
+
+# 定义输出目录和主类
+OUTPUT_DIR="output"
+MAIN_CLASS="org.zenoda.ztrader.ZtraderServer"
+
+# 查找output目录下所有的jar文件
+JAR_FILES=$(find $OUTPUT_DIR -name "*.jar" | paste -sd ":")
+
+# 检查是否找到了jar文件
+if [ -z "$JAR_FILES" ]; then
+  echo "没有在$output目录中找到任何jar文件。"
+  exit 1
+fi
+
+# 使用java命令运行应用
+java -cp "$JAR_FILES" $MAIN_CLASS
