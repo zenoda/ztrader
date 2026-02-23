281e5cd39f4659e30483416b4a6997a11af5f07b
chengda <chengda.cn@gmail.com>
2026-02-23 18:14:58 +0800

    平台基础框架

--- ztrader-client/src/views/profile/index.vue
+++ ztrader-client/src/views/profile/index.vue
@@ -4,12 +4,15 @@ import {getUserProfile, saveUserProfile, changePassword} from "@/lib/admin/UserC
 import {uploadFile} from "@/lib/file/FileClient.js"
 import AvatarDialog from "@/views/profile/AvatarDialog.vue";
 import ZPassword from "@/components/ZPassword.vue";
+import {ElMessage} from "element-plus";
 
 function onNavChange(item) {
   console.log(item)
 }
 
 const userProfile = ref({})
+const baseInfoForm = useTemplateRef('baseInfoForm')
+const changePasswordForm = useTemplateRef('changePasswordForm')
 onMounted(async () => {
   userProfile.value = await getUserProfile()
 })
@@ -35,15 +38,83 @@ function selectAvatar(url) {
 }
 
 async function onBaseInfoSaveClick() {
-  userProfile.value = await saveUserProfile(userProfile.value)
+  try {
+    await baseInfoForm.value.validate()
+  } catch (err) {
+    console.error(err)
+    ElMessage({type: 'error', message: '请检查输入内容'})
+    return
+  }
+  try {
+    userProfile.value = await saveUserProfile(userProfile.value)
+    ElMessage({type: 'success', message: '保存成功'})
+  } catch (err) {
+    console.error(err)
+    ElMessage({type: 'error', message: '保存失败'})
+  }
 }
 
-const passwordChangeRequest = ref({oldPassword: null, newPassword: null})
-const confirmPassword = ref()
+const passwordChangeRequest = ref({oldPassword: null, newPassword: null, confirmPassword: null})
 
 async function onChangePassword() {
-  userProfile.value = await changePassword(passwordChangeRequest.value)
+  try {
+    await changePasswordForm.value.validate()
+  } catch (err) {
+    console.error(err)
+    ElMessage({type: 'error', message: '请检查输入内容'})
+    return
+  }
+  try {
+    userProfile.value = await changePassword(passwordChangeRequest.value)
+    ElMessage({type: 'success', message: '密码修改成功'})
+  } catch (err) {
+    if (err.isAxiosError) {
+      ElMessage({type: 'error', message: `保存失败:${err.response.data.message}`})
+    } else {
+      console.error(err)
+      ElMessage({type: 'error', message: `保存失败`})
+    }
+  }
 }
+
+const baseInfoFormRules = ref({
+  nickname: {
+    required: true,
+    message: '请输入昵称'
+  },
+  phone: {
+    required: true,
+    message: '请输入手机号码'
+  },
+  email: {
+    required: true,
+    message: '请输入邮箱地址'
+  },
+  avatarUrl: {
+    required: true,
+    message: '请选择或上传头像'
+  }
+})
+
+const changePasswordFormRules = ref({
+  oldPassword: {
+    required: true,
+    message: '请输入当前密码'
+  },
+  newPassword: {
+    required: true,
+    message: '请输入新密码'
+  },
+  confirmPassword: [{
+    required: true,
+    message: '请再输入一次新密码进行确认'
+  }, {
+    validator(rule, value, callback) {
+      return value === passwordChangeRequest.value.newPassword
+    },
+    message: '确认密码要与新密码相同'
+  }]
+})
 </script>
 
 <template>
@@ -56,16 +127,17 @@ async function onChangePassword() {
         <el-row>
           <h3>基础信息</h3>
         </el-row>
-        <el-form class="user-profile-form" label-width="120px">
+        <el-form class="user-profile-form" label-width="120px" :model="userProfile" :rules="baseInfoFormRules"
+                 ref="baseInfoForm">
           <el-row>
             <el-col :span="12">
               <el-form-item label="用户名">
                 <el-input v-model="userProfile.username" disabled/>
               </el-form-item>
-              <el-form-item label="昵称">
+              <el-form-item label="昵称" prop="nickname">
                 <el-input v-model="userProfile.nickname"/>
               </el-form-item>
-              <el-form-item class="flex-input-item" label="电话号码">
+              <el-form-item class="flex-input-item" label="电话号码" prop="phone">
                 <el-input v-model="userProfile.phone"/>
                 <el-tag v-if="userProfile.phoneVerified" size="large" type="success">已验证</el-tag>
                 <el-tag v-else size="large" type="info">未验证</el-tag>
@@ -75,7 +147,7 @@ async function onChangePassword() {
                 <el-button>发送验证码</el-button>
                 <el-button type="primary">验证</el-button>
               </el-form-item>
-              <el-form-item class="flex-input-item" label="邮箱地址">
+              <el-form-item class="flex-input-item" label="邮箱地址" prop="email">
                 <el-input v-model="userProfile.email"/>
                 <el-tag v-if="userProfile.emailVerified" size="large" type="success">已验证</el-tag>
                 <el-tag v-else size="large" type="info">未验证</el-tag>
@@ -87,7 +159,7 @@ async function onChangePassword() {
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item class="user-avatar-item">
+              <el-form-item class="user-avatar-item" prop="avatarUrl">
                 <div class="flex direction-vertical items-center">
                   <div class="avatar-wrapper">
                     <el-avatar shape="square" :size="100" :src="userProfile.avatarUrl"/>
@@ -111,15 +183,16 @@ async function onChangePassword() {
         <el-row>
           <h3>修改密码</h3>
         </el-row>
-        <el-form class="change-password-form" label-width="120px">
-          <el-form-item label="当前密码">
+        <el-form class="change-password-form" label-width="120px" :rules="changePasswordFormRules"
+                 :model="passwordChangeRequest" ref="changePasswordForm">
+          <el-form-item label="当前密码" prop="oldPassword">
             <z-password v-model="passwordChangeRequest.oldPassword"/>
           </el-form-item>
-          <el-form-item label="新密码">
+          <el-form-item label="新密码" prop="newPassword">
             <z-password v-model="passwordChangeRequest.newPassword"/>
           </el-form-item>
-          <el-form-item label="确认新密码">
-            <z-password v-model="confirmPassword"/>
+          <el-form-item label="确认新密码" prop="confirmPassword">
+            <z-password v-model="passwordChangeRequest.confirmPassword"/>
           </el-form-item>
         </el-form>
         <el-row class="flex justify-end">
